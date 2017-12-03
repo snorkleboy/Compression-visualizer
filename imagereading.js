4e966e4c2a2abc953f325967f20787fa0dc9b8bb
Tim Kharshan <timkharshan@hotmail.com>
2017-12-03 14:08:51 -0800

    fixes, some refactoring, some stlying, and quadtree now activated by button

--- imagereading.js
+++ imagereading.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 class ImageReader{
     constructor(img){
         ///
-        //get convass that the image is loaded into and a blank canvass that I will put result onto
+        //get convass and result canvas and contexts
         const originalCanvass = document.getElementById('originalCanvass');
         const ctx = originalCanvass.getContext('2d');
         const resultCanvas = document.getElementById('result');
@@ -34,7 +34,7 @@ class ImageReader{
         ///
         //setup color picker
         const menuColor = document.getElementById('menu-color');
-        originalCanvass.addEventListener('mousemove', handleMouseMove(ctx,menuColor).bind(this));
+        resultCanvas.addEventListener('mousemove', handleMouseMove(resultCtx,menuColor).bind(this));
         ///
         //setup niaveCommression button and handler
         //on click gets imageData, value of inputs, validates input, and the calls niave compress of this.imageData, resultCtx, and input
@@ -42,7 +42,7 @@ class ImageReader{
         niaveButton.addEventListener('click', (e) => {
             //
             //get pixel data from original image and put into this.imageData
-            this.imageData = ctx.getImageData(0, 0, originalCanvass.width, originalCanvass.height);
+            const imageData = ctx.getImageData(0, 0, originalCanvass.width, originalCanvass.height);
             ///
             //get inputs
             let inX = parseInt(document.getElementById('niaveInputX').value);
@@ -60,91 +60,18 @@ class ImageReader{
             //call compression
             console.log("x,y,e", inY, inX, { x: inX || 10, y: inY || 10 }, expand);
             resultCtx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
-            this.NiaveCompress(this.imageData, resultCtx, { x: inX || 10, y: inY || 10 }, expand || 1, exval || 1);
+            NiaveCompress(imageData, resultCtx, { x: inX || 10, y: inY || 10 }, expand || 1, exval || 1);
         });
         
         console.log("start");
         //quad tree testing
-        this.imageData = ctx.getImageData(0, 0, originalCanvass.width, originalCanvass.height);
-        QuadtreeContainer(this.imageData,resultCtx);
-    }
+        const quadTreeSimpleButton = document.getElementById('quadtree');
+        const imageData = ctx.getImageData(0, 0, originalCanvass.width, originalCanvass.height);
+        quadTreeSimpleButton.addEventListener('click', handleQuadTreeClick(imageData, resultCtx));
 
-    NiaveCompress(imagedata, ctx, blockSize,expand, exval) {
-        // ctx.clearRect(0, 0, imagedata.width, imagedata.height);
-        // console.log("niavecompress", imagedata, ctx);
-        const data = imagedata.data;
-        //set/reset canvas width and height by blocksize. 
-        
-        if (expand === 2){
-            ctx.canvas.width = imagedata.width / blockSize.x;
-            ctx.canvas.height = imagedata.height / blockSize.y;
-        }else{
-            ctx.canvas.width = imagedata.width;
-            ctx.canvas.height = imagedata.height;
-        }
-        //delete previously made svg elements
-        if (expand === 5){
-            const myNode = document.getElementById('svgOne');
-            while (myNode.firstChild) {
-                myNode.removeChild(myNode.firstChild);
-            }
-        }
 
-        
-
-        for (let y = 0; y < imagedata.height; y = y + (blockSize.y)){
-            for (let x = 0; x < imagedata.width; x = x + (blockSize.x)){
-                const interval = setTimeout( ()=>{
-
-                    const i = ((y*imagedata.width) + x)*4;
-                    // console.log('x,y,i,data', { x: x, y: y },i, data[x + imagedata.width * 4 * y]);
-                    ctx.fillStyle = 'rgba(' + data[i] + ', ' + data[i+1] +
-                    ', ' + data[i+2] + ', ' + (data[i+3]) + ')';
-                    
-
-        
-                    //standard mode, fillrect of blocksize to color. color is set above to the top left pixel of block
-                    // defined by x*blocksize,y*blocksize
-                    if (expand===1){
-                        // (imagedata.height-y) type stuff to rotate
-                        ctx.fillRect(x,y, blockSize.x, blockSize.y);
-                        // ctx.fillStyle = 'black';
-                        // ctx.fillRect(x, y, blockSize.x, 1);
-                        // ctx.fillRect(x, y, 1, blockSize.y);
-                    
-                    //doesnt expand, fillrect of 1x1 (single pixel) of color as defined above into an image blocksize smaller.
-                    //ie if you have a blocksize of 2 the result will be an image half the size
-                    //the canvass id resized before the for loop to make the image downloadable as a small image
-                    }else if (expand===2){
-                        
-                        ctx.fillRect(x/blockSize.x, y/blockSize.y, 1, 1);
-
-                    //doesnt expand, but uses the same x, y coordinate of original image
-                    // this basically shows which pixel is being selected for each block
-                    }else if (expand===3){
-
-                        ctx.fillRect(x, y, exval, exval);
-                    //expands by making a circle of radius=the averge of blocksize.x and blocksize.y, fills with color defined above.
-                    //the optional expandValue attribute changes how much less than the average the circle is filled by, so larget value means saller circle
-                    }else if (expand===4){     
-                        ctx.beginPath();
-                        ctx.arc(x, y, (blockSize.x + blockSize.y) / (2 * exval), 0, 2 * Math.PI, false);
-                        ctx.fill();
-                    }else if (expand ===5){
-                        // console.log('here')
-                        let svgns = "http://www.w3.org/2000/svg";
-                        let rect = document.createElementNS(svgns, 'rect');
-                        rect.setAttributeNS(null, 'x', x*5);
-                        rect.setAttributeNS(null, 'y', y*5);
-                        rect.setAttributeNS(null, 'height', 5*blockSize.y);
-                        rect.setAttributeNS(null, 'width', 5*blockSize.x);
-                        rect.setAttributeNS(null, 'fill', ctx.fillStyle);
-                        document.getElementById('svgOne').appendChild(rect);
-                    }                
-                },1000+(x*10+y*imagedata.width)/100);}
-            
-
-        }
+       
+        // QuadtreeContainer(this.imageData,resultCtx);
     }
 
 }
@@ -171,6 +98,91 @@ function makeDownload(imageData, element){
     link.setAttribute('href', imageData);
     link.setAttribute('download', 'myimage.png');
     dlLinkDiv.appendChild(link);
+}
+
+function handleQuadTreeClick(imageData, context){
+    return (e) => {
+        new QuadtreeContainer(imageData,context);
+    };
+}
+
+function NiaveCompress(imagedata, ctx, blockSize, expand, exval) {
+    // ctx.clearRect(0, 0, imagedata.width, imagedata.height);
+    // console.log("niavecompress", imagedata, ctx);
+    const data = imagedata.data;
+    //set/reset canvas width and height by blocksize. 
+
+    if (expand === 2) {
+        ctx.canvas.width = imagedata.width / blockSize.x;
+        ctx.canvas.height = imagedata.height / blockSize.y;
+    } else {
+        ctx.canvas.width = imagedata.width;
+        ctx.canvas.height = imagedata.height;
+    }
+    //delete previously made svg elements
+    if (expand === 5) {
+        const myNode = document.getElementById('svgOne');
+        while (myNode.firstChild) {
+            myNode.removeChild(myNode.firstChild);
+        }
+    }
+
+
+
+    for (let y = 0; y < imagedata.height; y = y + (blockSize.y)) {
+        for (let x = 0; x < imagedata.width; x = x + (blockSize.x)) {
+            const interval = setTimeout(() => {
+
+                const i = ((y * imagedata.width) + x) * 4;
+                // console.log('x,y,i,data', { x: x, y: y },i, data[x + imagedata.width * 4 * y]);
+                ctx.fillStyle = 'rgba(' + data[i] + ', ' + data[i + 1] +
+                    ', ' + data[i + 2] + ', ' + (data[i + 3]) + ')';
+
+
+
+                //standard mode, fillrect of blocksize to color. color is set above to the top left pixel of block
+                // defined by x*blocksize,y*blocksize
+                if (expand === 1) {
+                    // (imagedata.height-y) type stuff to rotate
+                    ctx.fillRect(x, y, blockSize.x, blockSize.y);
+                    // ctx.fillStyle = 'black';
+                    // ctx.fillRect(x, y, blockSize.x, 1);
+                    // ctx.fillRect(x, y, 1, blockSize.y);
+
+                    //doesnt expand, fillrect of 1x1 (single pixel) of color as defined above into an image blocksize smaller.
+                    //ie if you have a blocksize of 2 the result will be an image half the size
+                    //the canvass id resized before the for loop to make the image downloadable as a small image
+                } else if (expand === 2) {
+
+                    ctx.fillRect(x / blockSize.x, y / blockSize.y, 1, 1);
+
+                    //doesnt expand, but uses the same x, y coordinate of original image
+                    // this basically shows which pixel is being selected for each block
+                } else if (expand === 3) {
+
+                    ctx.fillRect(x, y, exval, exval);
+                    //expands by making a circle of radius=the averge of blocksize.x and blocksize.y, fills with color defined above.
+                    //the optional expandValue attribute changes how much less than the average the circle is filled by, so larget value means saller circle
+                } else if (expand === 4) {
+                    ctx.beginPath();
+                    ctx.arc(x, y, (blockSize.x + blockSize.y) / (2 * exval), 0, 2 * Math.PI, false);
+                    ctx.fill();
+                } else if (expand === 5) {
+                    // console.log('here')
+                    let svgns = "http://www.w3.org/2000/svg";
+                    let rect = document.createElementNS(svgns, 'rect');
+                    rect.setAttributeNS(null, 'x', x * 5);
+                    rect.setAttributeNS(null, 'y', y * 5);
+                    rect.setAttributeNS(null, 'height', 5 * blockSize.y);
+                    rect.setAttributeNS(null, 'width', 5 * blockSize.x);
+                    rect.setAttributeNS(null, 'fill', ctx.fillStyle);
+                    document.getElementById('svgOne').appendChild(rect);
+                }
+            }, 1000 + (x * 10 + y * imagedata.width) / 100);
+        }
+
+
+    }
 }
 
 
@@ -202,6 +214,11 @@ function QuadtreeContainer(imageData, context){
         this.nodes = [];
         
         context.fillStyle = this.color;
+
+        context.beginPath();
+        context.arc(this.mpX, this.mpY, (bounds.width), 0, 2 * Math.PI, false);
+        context.fill();
+
         context.clearRect(bounds.x, bounds.y, bounds.width, bounds.height);
         context.fillRect(bounds.x,bounds.y,bounds.width,bounds.height);
         // console.log(this);
