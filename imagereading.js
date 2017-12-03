bde97b2f00dcd4d1f102fdba2c170cb9768d36b9
Tim Kharshan <timkharshan@hotmail.com>
2017-12-02 22:21:03 -0800

    used set interval to make animated

--- imagereading.js
+++ imagereading.js
@@ -21,29 +21,20 @@ class ImageReader{
         const ctx = originalCanvass.getContext('2d');
         const resultCanvas = document.getElementById('result');
         const resultCtx = resultCanvas.getContext('2d');
-
-        
         ///
         //turn off anti aliasing to see pixels
         ctx.imageSmoothingEnabled = false;
         resultCtx.imageSmoothingEnabled = false;
-
         ///
         //draw original image
         ctx.drawImage(img,0,0);
-        
         ///
         //draw original image onto result canvass
         // resultCtx.drawImage(originalCanvass, 0, 0);
-
-        ///
-        
-
         ///
         //setup color picker
         const menuColor = document.getElementById('menu-color');
         originalCanvass.addEventListener('mousemove', handleMouseMove(ctx,menuColor).bind(this));
-
         ///
         //setup niaveCommression button and handler
         //on click gets imageData, value of inputs, validates input, and the calls niave compress of this.imageData, resultCtx, and input
@@ -52,37 +43,33 @@ class ImageReader{
             //
             //get pixel data from original image and put into this.imageData
             this.imageData = ctx.getImageData(0, 0, originalCanvass.width, originalCanvass.height);
-
             ///
             //get inputs
-
             let inX = parseInt(document.getElementById('niaveInputX').value);
             let inY = parseInt(document.getElementById('niaveInputY').value);
             let expand = parseInt(document.getElementById('niaveInputExpand').value);
-            let exval = parseFloat(document.getElementById('niaveInputExpandval').value);
-            
+            let exval = parseFloat(document.getElementById('niaveInputExpandval').value);           
             //
-            //validations
-            // console.log("x,y,e", inY, inX, { x: inX || 10, y: inY || 10 }, expand);
-            if (expand>4 || expand<1){
+            //validations           
+            if (expand>5 || expand<1){
                 expand = 1;
             }
             if (inY < 1)inY = 1;
-            if (inX < 1) inY = 1;
-           
+            if (inX < 1) inY = 1;           
             ///
             //call compression
+            console.log("x,y,e", inY, inX, { x: inX || 10, y: inY || 10 }, expand);
+            resultCtx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
             this.NiaveCompress(this.imageData, resultCtx, { x: inX || 10, y: inY || 10 }, expand || 1, exval || 1);
-        });
-        
+        });       
     }
 
     NiaveCompress(imagedata, ctx, blockSize,expand, exval) {
-        ctx.clearRect(0, 0, imagedata.width, imagedata.height);
+        // ctx.clearRect(0, 0, imagedata.width, imagedata.height);
         // console.log("niavecompress", imagedata, ctx);
-
-        //set/reset canvas width and height by blocksize. 
         const data = imagedata.data;
+        //set/reset canvas width and height by blocksize. 
+        
         if (expand === 2){
             ctx.canvas.width = imagedata.width / blockSize.x;
             ctx.canvas.height = imagedata.height / blockSize.y;
@@ -90,14 +77,25 @@ class ImageReader{
             ctx.canvas.width = imagedata.width;
             ctx.canvas.height = imagedata.height;
         }
+        //delete previously made svg elements
+        if (expand === 5){
+            const myNode = document.getElementById('svgOne');
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+        }
+
+        
 
         for (let y = 0; y < imagedata.height; y = y + (blockSize.y)){
-            for (let x = 0; x < imagedata.width * 4; x = x + (blockSize.x)){
-                const i = ((y*imagedata.width) + x)*4;
+            for (let x = 0; x < imagedata.width; x = x + (blockSize.x)){
+                const interval = setInterval(()=>{const i = ((y*imagedata.width) + x)*4;
                 // console.log('x,y,i,data', { x: x, y: y },i, data[x + imagedata.width * 4 * y]);
                 ctx.fillStyle = 'rgba(' + data[i] + ', ' + data[i+1] +
                  ', ' + data[i+2] + ', ' + (data[i+3]) + ')';
+                
 
+    
                 //standard mode, fillrect of blocksize to color. color is set above to the top left pixel of block
                 // defined by x*blocksize,y*blocksize
                 if (expand===1){
@@ -109,6 +107,7 @@ class ImageReader{
                 
                 //doesnt expand, fillrect of 1x1 (single pixel) of color as defined above into an image blocksize smaller.
                 //ie if you have a blocksize of 2 the result will be an image half the size
+                //the canvass id resized before the for loop to make the image downloadable as a small image
                 }else if (expand===2){
                     
                     ctx.fillRect(x/blockSize.x, y/blockSize.y, 1, 1);
@@ -116,6 +115,7 @@ class ImageReader{
                  //doesnt expand, but uses the same x, y coordinate of original image
                 // this basically shows which pixel is being selected for each block
                 }else if (expand===3){
+
                     ctx.fillRect(x, y, exval, exval);
                 //expands by making a circle of radius=the averge of blocksize.x and blocksize.y, fills with color defined above.
                 //the optional expandValue attribute changes how much less than the average the circle is filled by, so larget value means saller circle
@@ -123,17 +123,28 @@ class ImageReader{
                     ctx.beginPath();
                     ctx.arc(x, y, (blockSize.x + blockSize.y) / (2 * exval), 0, 2 * Math.PI, false);
                     ctx.fill();
+                }else if (expand ===5){
+                    // console.log('here')
+                    let svgns = "http://www.w3.org/2000/svg";
+                    let rect = document.createElementNS(svgns, 'rect');
+                    rect.setAttributeNS(null, 'x', x*5);
+                    rect.setAttributeNS(null, 'y', y*5);
+                    rect.setAttributeNS(null, 'height', 5*blockSize.y);
+                    rect.setAttributeNS(null, 'width', 5*blockSize.x);
+                    rect.setAttributeNS(null, 'fill', ctx.fillStyle);
+                    document.getElementById('svgOne').appendChild(rect);
                 }
+                    clearInterval(interval);
 
                 
-            }
+            },1000/15);}
+            
 
         }
     }
 
 }
 
-function handleNiaveCompress(){}
 
 function handleMouseMove(ctx, element){
     return ((event) => {
@@ -160,3 +171,14 @@ function makeDownload(imageData, element){
 
 
 
+function Quadtree(bounds, max_objects, max_levels, level) {
+
+    this.max_objects = max_objects || 10;
+    this.max_levels = max_levels || 4;
+
+    this.level = level || 0;
+    this.bounds = bounds;
+
+    this.objects = [];
+    this.nodes = [];
+};
