1a8bb85d12e7c4b7e5dc3fa33892dbc4dec85b75
Tim Kharshan <timkharshan@hotmail.com>
2018-01-12 13:16:19 -0800

    fades working

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,4 @@
 import { DemoObj,DemoRunner} from '../assets/demo';
-import { setTimeout } from 'timers';
 
 const intro = () => `
 <div class='aa'>
@@ -36,38 +35,42 @@ const three = () => `
 const four = () => {
 
     return `
-<div class='aa'>
- <h2> 4</h2>
-  <h2> back</h2>
-  <h1> fo mo</h1>
-  <button onclick="demo.stay()">stay</button>
-  <button onclick="demo.destroy()">destroy</button>
-  <button onclick="demo.goBack()">goBack</button>
-  <button onClick="demo.end()">end demo</button>
-</div>
-`};
+            <div class='aa'>
+            <h2> 4</h2>
+            <h2> back</h2>
+            <h1> fo mo</h1>
+            <button onclick="demo.stay()">stay</button>
+            <button onclick="demo.destroy()">destroy</button>
+            <button onclick="demo.goBack()">goBack</button>
+            <button onClick="demo.end()">end demo</button>
+            </div>
+            `
+};
 const fadeIn = function(el){
     const body = document.querySelector('body');
     body.appendChild(el);
-
+    el.style.transition = 'opacity .3s';
+    el.style.opacity = '0';
+    setTimeout(() => { el.style.opacity = '1'; }, 0);
 };
 
 const fadeOut = function(next,el){
     const body = document.querySelector('body');
     console.log(el);
-
-    el.style.opacity ='0';
+    el.style.transition = 'opacity .3s';
+    el.style.opacity = '1';
+    setTimeout(() => { el.style.opacity = '0'; }, 0);
     setTimeout(()=>{
         body.removeChild(el);
         next();
-    },1010)
+    },400);
 };
 
 const demo = [
-    new DemoObj(intro, fadeIn, fadeOut, 4000),
-    new DemoObj(two, fadeIn, fadeOut, 4000),
-    new DemoObj(three, fadeIn, fadeOut, 4000),
-    new DemoObj(four, fadeIn, fadeOut, 4000),
+    new DemoObj(intro, fadeIn, fadeOut, 2000),
+    new DemoObj(two, fadeIn, fadeOut, 2000),
+    new DemoObj(three, fadeIn, fadeOut, 2000),
+    new DemoObj(four, fadeIn, fadeOut, 2000),
 ];
 
 export default new DemoRunner(demo);
